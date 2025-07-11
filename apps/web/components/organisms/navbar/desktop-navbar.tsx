f0858bce7d253df6819896a544b08ef0fe38a1ef
M. Yudistiandy Prabowo <andymyp1997@gmail.com>
2025-07-11 10:30:05 +0700

    web: is active

--- apps/web/components/organisms/navbar/desktop-navbar.tsx
+++ apps/web/components/organisms/navbar/desktop-navbar.tsx
@@ -11,21 +11,30 @@ import { fallbackAvatarColor, fallbackAvatarName } from "@/lib/avatar";
 import { AppState } from "@/lib/store";
 import { Bell, Bookmark, Home, Mail, User, Users } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useSelector } from "react-redux";
 
 export function DesktopNavbar() {
-  const menuList = [
-    { icon: Home, label: "Home", active: true },
-    { icon: User, label: "Profile" },
-    { icon: Users, label: "Communities" },
-    { icon: Bell, label: "Notifications" },
-    { icon: Mail, label: "Messages" },
-    { icon: Bookmark, label: "Bookmarks" },
-  ];
-
+  const pathname = usePathname();
   const user = useSelector((state: AppState) => state.auth.user);
 
   if (!user) return <LoadingSkeleton />;
+
+  const menuList = [
+    { icon: Home, label: "Home", link: "/" },
+    { icon: User, label: "Profile", link: `/profile/${user.username}` },
+    { icon: Users, label: "Communities", link: "/communities" },
+    { icon: Bell, label: "Notifications", link: "/notifications" },
+    { icon: Mail, label: "Messages", link: "/messages" },
+    { icon: Bookmark, label: "Bookmarks", link: "/bookmarks" },
+  ];
+
+  const isActive = (link: string) => {
+    const pathBase = pathname.split("/")[1];
+    const linkBase = link.split("/")[1];
+
+    return pathBase === linkBase;
+  };
 
   return (
     <Card className="shadow-md shadow-violet-500/5 bg-white/80 dark:bg-gray-900/80 backdrop-blur-sm overflow-hidden py-0">
@@ -51,16 +60,16 @@ export function DesktopNavbar() {
           {menuList.map((item) => (
             <Link
               key={item.label}
-              href="#"
+              href={item.link}
               className={`group flex items-center px-3 py-3 text-sm font-medium rounded-xl transition-all duration-200 ${
-                item.active
+                isActive(item.link)
                   ? "bg-gradient-to-r from-violet-500 to-purple-500 text-white"
                   : "text-gray-700 dark:text-gray-300 hover:bg-violet-50 dark:hover:bg-violet-950/50 hover:text-violet-600 dark:hover:text-violet-400"
               }`}
             >
               <item.icon
                 className={`mr-3 h-5 w-5 transition-transform duration-200 ${
-                  item.active ? "" : "group-hover:scale-110"
+                  isActive(item.link) ? "" : "group-hover:scale-110"
                 }`}
               />
               {item.label}
